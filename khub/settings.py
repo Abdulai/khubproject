a6b24442b90865a6b899c0f80fbec07e1b095ac2
Abdulai <abdulbah40@hotmail.co.uk>
2020-09-01 16:23:10 +0000

    events and training with index image updated

--- khub/settings.py
+++ khub/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'news.apps.NewsConfig',
+    'eventstraining.apps.EventstrainingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- khub/urls.py
+++ khub/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('pages.urls')),
     path('news', include('news.urls')),
+    path('eventstraining', include('eventstraining.urls')),
     path('admin/', admin.site.urls),
 ]
