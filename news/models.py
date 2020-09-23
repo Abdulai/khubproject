2d30225e4842366545d3f9d955038340e11c4241
Abdulai <abdulbah40@hotmail.co.uk>
2020-09-23 16:35:10 +0000

    gitignore updated

--- news/models.py
+++ news/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    seats_available = models.IntegerField(blank=True)
+    date = models.DateField()
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Page(models.Model):
+    name = models.CharField(max_length=100)
+    area_of_study = models.CharField(max_length=200)
+    project_completed = models.CharField(max_length=400)
+    ongoing_project = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
